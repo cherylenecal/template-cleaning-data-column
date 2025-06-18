c65151caccc92c31fce63d25e441370fd8e38a83
cherylenecal <rehcenel@gmail.com>
2025-06-18 09:01:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,9 @@ def move_to_template(df):
     upper_columns = ["RoomOption", "TreatmentPlace", "PrimaryDiagnosis"]
     for col in upper_columns:
         new_df[col] = new_df[col].str.upper()
-    new_df.loc[new_df["RoomOption"] == "ON PLAN", "RoomOption"] = "ONPLAN"
+    if "RoomOption" in new_df.columns:
+        new_df["RoomOption"] = new_df["RoomOption"].astype(str).str.strip().str.upper()
+        new_df.loc[new_df["RoomOption"] == "ON PLAN", "RoomOption"] = "ONPLAN"
 
     # Step 5: Transform to the new template
     df_transformed = pd.DataFrame({
